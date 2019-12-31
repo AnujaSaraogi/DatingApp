b419dbcbdde8767820d5204504ff83540f1a8322
Anuja Saraogi <saraanuja1489@gmail.com>
2019-12-31 23:19:07 +0530

    End of Security Section

---
+++ DatingApp.API/Dtos/UserToLoginDto.cs
@@ -0,0 +1,11 @@
+
+namespace DatingApp.API.Dtos
+{
+    public class UserToLoginDto
+    {
+        
+        public string Username { get; set; }
+      
+        public string Password { get; set; }
+    }
+}
